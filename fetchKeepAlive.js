9d6ebce0de3bf7f3a6826a60fda1976fc0dcbabc
agungd3v <agungd3v@gmail.com>
2020-12-04 06:04:46 +0700

    get from promise

--- fetchKeepAlive.js
+++ fetchKeepAlive.js
@@ -1,8 +1,18 @@
 class fetchKeepAlive {
-  constructor({ msg }) {
-    console.log(msg)
-  }
-  static get(msg) {
-    return new fetchKeepAlive(msg)
+  async get({ url }) {
+    await fetch(url, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }).then(res => res.json()).then(data => {
+      Promise.resolve(data).then(value => {
+        console.log(value)
+      }).catch(e => {
+        console.error('Oops, something wrong from Promise please wait... ' + e)
+      })
+    }).catch(err => {
+      console.error('Oops, ' + err)
+    })
   }
 }
