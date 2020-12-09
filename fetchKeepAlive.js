1737166d4f71f6db3c0899ce952d84aa31b71f3f
agungd3v <agungd3v@gmail.com>
2020-12-09 18:07:35 +0700

    showing popup with condition window

--- fetchKeepAlive.js
+++ fetchKeepAlive.js
@@ -3,6 +3,7 @@ class fetchKeepAlive {
     this.data = data
     this.selector = selector
     this.pattern = /^((http|https):\/\/)/;
+    this.show = true
 
     if (this.data === undefined) return console.error('Oops, you forgot pass the data😱')
     if (!this.pattern.test(this.data)) return console.error('Oops, data must be from http or https😅')
@@ -25,13 +26,70 @@ class fetchKeepAlive {
     if (element.tagName !== 'DIV') {
       return console.error('Oops, element must be <div>🙃')
     }
+    Object.assign(element.style, {
+      width: "100%",
+      height: "100%",
+      position: "fixed",
+      left: 0,
+      top: 0,
+      backgroundColor: "rgba(0, 0, 0, 0.625)",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center"
+    })
     dt.forEach(data => {
-      element.innerHTML += this.appendIn(data.image, data.width, data.height)
+      const appendIn = this.appendIn(data.image, data.width, data.height)
+      element.innerHTML += appendIn === undefined ? element.remove() : appendIn
+    })
+    element.querySelectorAll('button').forEach((e, i) => {
+      const endIndex = 0
+      e.addEventListener('click', function() {
+        if (i === endIndex) element.remove()
+        this.parentNode.parentNode.remove()
+      })
     })
   }
   appendIn(src, width, height) {
-    return `
-      <img src="${src}" width="${width}" height="${height}"></img>
-    `
+    const styles = this.styleString(width, height)
+    if (width <= window.innerWidth) {
+      return `
+        <div style="${styles.wrapper}">
+          <div style="position: relative">
+            <button style="${styles.close}">x</button>
+            <img src="${src}" width="${width - 50}" height="${height - 50}"></img>
+          </div>
+        </div>
+      `
+    }
+  }
+  styleString(w, h) {
+    const wrapper = {
+      "position": "absolute",
+      "display": "flex",
+      "justify-content": "center",
+      "align-items": "center",
+      "width": w + "px",
+      "height": h + "px"
+    }
+    const close = {
+      "position": "absolute",
+      "top": "-20px",
+      "right": "-20px",
+      "width": w >= window.innerWidth ? w / 10 + "px" : "40px",
+      "height": w >= window.innerWidth ? w / 10 + "px" : "40px",
+      "border-radius": "100%",
+      "outline": "none",
+      "border": "none",
+      "background-color": "gold",
+      "font-size": w >= window.innerWidth ? w / 15 + "px" : "20px",
+      "cursor": "pointer"
+    }
+    let styles = {
+      wrapper: '',
+      close: ''
+    }
+    for (let key in wrapper) { styles.wrapper += key + ": " + wrapper[key] + "; " }
+    for (let key in close) { styles.close += key + ": " + close[key] + "; " }
+    return styles
   }
 }
